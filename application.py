576dbb6224c533d71647b147913b21b01cd06b35
Larry Tooley <larry@larrytooley.com>
2015-11-05 15:11:15 -0500

    Created basic pages.

--- application.py
+++ application.py
@@ -16,24 +16,21 @@ session = DBSession()
 
 
 @app.route('/')
+@app.route('/booths/')
 def index():
     booths = session.query(Booths).order_by(asc(Booths.name))
     return render_template('index.html', booths=booths)
 
 
-@app.route('/booths/')
-def booths():
-    booths = session.query(Booths).order_by(asc(Booths.name))
-    return render_template('booths.html', booths=booths)
+@app.route('/booth/<int:booth_id>/')
+def booth(booth_id=None):
+    booth = session.query(Booths).filter_by(id=booth_id).one()
+    items = session.query(Items).filter_by(booth_id=booth_id)
+    return render_template('booth.html', booth=booth, items=items)
 
-@app.route('/booths/<name>/')
-def booth(name=None):
-    return render_template('booth.html', name=name)
-
-
-@app.route('/<name>/<item>/')
-def item(name=None, item=None):
-    return render_template('item.html', name=name, item=item)
+@app.route('/add/')
+def addItem(booth_id = None):
+    return render_template('addItem.html')
 
 
 @app.route('/login/')
