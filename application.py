e32e51dfaf4d90aaa5f57b51334487de16b5170c
Larry Tooley <larry@larrytooley.com>
2015-11-06 07:13:04 -0500

    Added an all items page

--- application.py
+++ application.py
@@ -18,19 +18,41 @@ session = DBSession()
 @app.route('/')
 @app.route('/booths/')
 def index():
+    ''' Add comment here '''
     booths = session.query(Booths).order_by(asc(Booths.name))
     return render_template('index.html', booths=booths)
 
 
 @app.route('/booth/<int:booth_id>/')
 def booth(booth_id=None):
+    ''' Add comment here '''
     booth = session.query(Booths).filter_by(id=booth_id).one()
     items = session.query(Items).filter_by(booth_id=booth_id)
     return render_template('booth.html', booth=booth, items=items)
 
 @app.route('/add/')
 def addItem(booth_id = None):
+    ''' Add comment here '''
     return render_template('addItem.html')
+
+
+@app.route('/items/')
+def allItems(booth_id = None):
+    ''' Add comment here '''
+    items = session.query(Items).order_by(asc(Items.name))
+    return render_template('items.html', items=items)
+
+
+# @app.route('/add/')
+# def addItem(booth_id = None):
+#     ''' Add comment here '''
+#     return render_template('addItem.html')
+#
+#
+# @app.route('/add/')
+# def addItem(booth_id = None):
+#     ''' Add comment here '''
+#     return render_template('addItem.html')
 
 
 @app.route('/login/')
