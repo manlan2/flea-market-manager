420cd6bc748406854dfaffdf47bf17fd47ece516
Larry Tooley <larry@larrytooley.com>
2015-11-06 07:48:03 -0500

    Added endpoints for CRUD.

--- application.py
+++ application.py
@@ -22,19 +22,6 @@ def index():
     booths = session.query(Booths).order_by(asc(Booths.name))
     return render_template('index.html', booths=booths)
 
-# Show booth items and info
-@app.route('/booth/<int:booth_id>/')
-def booth(booth_id=None):
-    ''' Add comment here '''
-    booth = session.query(Booths).filter_by(id=booth_id).one()
-    items = session.query(Items).filter_by(booth_id=booth_id)
-    return render_template('booth.html', booth=booth, items=items)
-
-# Add item
-@app.route('/booth/<int:booth_id>/new/')
-def addItem(booth_id = None):
-    ''' This function is used to create new items for a booth.'''
-    return render_template('addItem.html')
 
 # List of all items
 @app.route('/items/')
@@ -44,16 +31,42 @@ def allItems(booth_id = None):
     return render_template('items.html', items=items)
 
 
-# @app.route('/add/')
-# def addItem(booth_id = None):
-#     ''' Add comment here '''
-#     return render_template('addItem.html')
-#
-#
-# @app.route('/add/')
-# def addItem(booth_id = None):
-#     ''' Add comment here '''
-#     return render_template('addItem.html')
+# Show booth items and info
+@app.route('/booth/<int:booth_id>/')
+def booth(booth_id=None):
+    ''' Add comment here '''
+    booth = session.query(Booths).filter_by(id=booth_id).one()
+    items = session.query(Items).filter_by(booth_id=booth_id)
+    return render_template('booth.html', booth=booth, items=items)
+
+
+# Single item view #TODO: this is not working.  No item sent to template?
+@app.route('/booth/<int:booth_id>/<int:item_id>/')
+def item(booth_id=None, item_id = None):
+    ''' Add comment here '''
+    booth = session.query(Booths).filter_by(id=booth_id).one()
+    item = session.query(Items).filter_by(id=item_id)
+    return render_template('item.html', booth=booth, item=item)
+
+
+# Add item
+@app.route('/booth/<int:booth_id>/new/')
+def addItem(booth_id = None):
+    ''' This function is used to create new items for a booth.'''
+    return render_template('addItem.html')
+
+
+# Edit item
+@app.route('/booth/<int:booth_id>/<int:item_id>/edit/')
+def editItem(booth_id = None, item_id = None):
+    ''' Add comment here '''
+    return render_template('editItem.html')
+
+# Delete item
+@app.route('/booth/<int:booth_id>/<int:item_id>/delete/')
+def deleteItem(booth_id = None, item_id = None):
+    ''' Add comment here '''
+    return render_template('deleteItem.html')
 
 
 @app.route('/login/')
